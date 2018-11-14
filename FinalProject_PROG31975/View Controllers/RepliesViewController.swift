237cb5bd2d07b005387af95139e86188d8b4fdac
Devon Fazekas <thomas-d@hotmail.ca>
2018-11-14 14:58:38 -0500

    Added Replies ViewController.

---
+++ FinalProject_PROG31975/View Controllers/RepliesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RepliesViewController.swift
+//  FinalProject_PROG31975
+//
+//  Created by Devon on 2018-11-14.
+//  Copyright © 2018 PROG31975. All rights reserved.
+//
+
+import UIKit
+
+class RepliesViewController: UIViewController {
+
+    
+    ///// Misc. content.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
